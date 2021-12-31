9f802029a39f887467ecd11ed5b7df31c776f850
Keren Guez <keren084@gmail.com>
2021-12-31 17:46:48 +0200

    changed ls http method to post and added an integration test for it

--- routes/ls.js
+++ routes/ls.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const authUser = require("../middleware/authUser");
 const authFile = require("../middleware/authFile");
+const envConstants = require("../configs/envConstants.json");
 
 const router = express.Router();
 
-router.get("/:userName/:filePath(*)", authUser, authFile, (req, res) => {
-  const { file } = req.file;
-  const resultText = file.content.map( currentFile => ({ name: currentFile.name, type: currentFile.type }) );
-  res.send(JSON.stringify(resultText));
+router.post("/:userName", authUser, authFile, (req, res) => {
+  const lsResult = req.file.reduce((accumulator, currentFile, currentIndex) => {
+    const currentFilePath = req.body.params[currentIndex];
+
+    if (currentFile.type === envConstants.types.f)
+      return accumulator + "\n" + currentFilePath;
+
+    if (req.file.length > 1 && currentFile.type === envConstants.types.d)
+      accumulator += `${currentFilePath}:\n`;
+
+    return (
+      accumulator +
+      currentFile.content
+        .map((fileInDir) => `name: ${fileInDir.name}, type: ${fileInDir.type}`)
+        .join("\n") + "\n"
+    );
+  }, "");
+
+  res.send(lsResult.slice(0));
 });
 
 module.exports = router;
---
+++ tests/integration/ls.test.js
@@ -0,0 +1,55 @@
+const request = require("supertest");
+let server;
+
+describe("/api/v1/ls/keren", () => {
+    beforeEach(() => {
+        server = require("../../index").server;
+    });
+
+    afterEach(async () => {
+    server.close();
+    });
+    
+      describe("POST /", () => {
+        let filePaths = ['/root']
+        const filesInRoot = `name: file1.txt, type: file\nname: file2.txt, type: file\nname: file3.txt, type: file\n`;
+
+        const exec = async () => {
+            return await request(server)
+                .post("/api/v1/ls/keren")
+                .send({ params: filePaths });
+        }
+    
+        it("should return all files that are in the given folder", async () => {
+            const res = await exec();
+    
+            expect(res.status).toBe(200);
+            expect(res.text).toBe(filesInRoot);
+        });
+
+        it("should return the given path if a file is given instead of a directory", async () => {
+            const filePath = '/root/file1.txt';
+            filePaths = [filePath]
+            const res = await exec();
+    
+            expect(res.status).toBe(200);
+            expect(res.text).toBe("\n" + filePath);
+        });
+
+        it("should return all files that are in the folders", async () => {
+            filePaths = ['/root', '/bin', '/root/file1.txt'];
+            const filesInRootAndBin = '/root:\n'+ filesInRoot + '/bin:\n\n\n' + '/root/file1.txt'
+            const res = await exec();
+    
+            expect(res.status).toBe(200);
+            expect(res.text).toBe(filesInRootAndBin);
+        });
+
+        it("should return 404 if the file doesn't exist", async () => {
+            filePaths = ['noneExistingFolder'];
+            const res = await exec();
+    
+            expect(res.status).toBe(404);
+        });
+    }) 
+});
